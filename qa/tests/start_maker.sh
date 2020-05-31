b7f33760376bbd4a812ea792591dd1340469a754
SirSevenG <ottonseven@gmail.com>
2020-06-01 03:45:30 +0700

    revert mm2 containers start

---
+++ qa/tests/start_maker.sh
@@ -0,0 +1,3 @@
+snode=`dig +short mm_seed`
+export config=`echo \{\"gui\":\"nogui\",\"netid\":9012,\"userhome\":\"${HOME}/\",\"passphrase\":\"Utovd1JeRZ8jKGRQgUyxrFvGKEck9danoTqZRHfB7MBXpTUqB6vr\",\"rpcip\":\"0.0.0.0\",\"rpc_password\":\"RPC_PASSWORD\",\"rpc_local_only\":false,\"seednodes\":[\"$snode\"\]\}`
+mm2 "${config}"
---
+++ qa/tests/start_seed.sh
@@ -0,0 +1,2 @@
+export config=`echo \{\"gui\":\"nogui\",\"netid\":9012,\"userhome\":\"${HOME}/\",\"passphrase\":\"Uuw5XXdTCjice4S6buknpcjJ6KQJQTw4ZHCrbeUkJXx8ESSGhfBa\",\"rpcip\":\"0.0.0.0\",\"rpc_password\":\"RPC_PASSWORD\",\"rpc_local_only\":false,\"i_am_seed\":true\}`
+mm2 "${config}"
