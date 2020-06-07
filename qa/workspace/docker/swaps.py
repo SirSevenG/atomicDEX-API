d30978ac641f2517ceeb13ab1e1ae847994f28c9
SirSevenG <ottonseven@gmail.com>
2020-06-08 04:53:39 +0700

    qa swaps fix math

--- qa/workspace/docker/swaps.py
+++ qa/workspace/docker/swaps.py
@@ -1,11 +1,13 @@
 from test_pylibs.test_utils import init_logs, enable_electrums, swap_status_iterator, init_connection, rand_value
 import time
+from decimal import Decimal
 import pytest
 
 
 def mainloop(maker: object, taker: object, coin_a: str, coin_b: str, log: object):
     time_sleep = 30
     swap_uuids = []
+    swaps_to_run = 3
     log.info("Entering main test loop")
     log.debug("Clearing up previous orders in %s s", str(time_sleep))
     maker.cancel_all_orders(cancel_by={'type': 'All'})  # reset orders
@@ -13,11 +15,12 @@ def mainloop(maker: object, taker: object, coin_a: str, coin_b: str, log: object
     time.sleep(time_sleep)
     price1 = rand_value(0.81, 0.995)  # gen prices and volumes for swap
     volume1 = rand_value(0.5, 0.9)
+    volume_to_swap = "{0:.8f}".format((Decimal(volume1)*Decimal(0.9))/swaps_to_run)
     log.info("Creating maker order in %s s", str(time_sleep))
     res = maker.setprice(base=coin_a, rel=coin_b, price=price1, volume=volume1, cancel_previous=False)
     log.debug("Response: %s", str(res))
-    for i in range(3):
-        resp = taker.buy(base=coin_a, rel=coin_b, price=price1, volume=volume1)
+    for i in range(swaps_to_run):
+        resp = taker.buy(base=coin_a, rel=coin_b, price=price1, volume=volume_to_swap)
         log.debug("Create order, number: %s\n%s", str(i + 1), str(resp))
         if resp.get("result"):
             swap_uuids.append((resp.get("result")).get("uuid"))
