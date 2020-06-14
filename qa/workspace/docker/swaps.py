b317b95966504665e70f65b2d695eada192156d1
SirSevenG <ottonseven@gmail.com>
2020-06-15 00:25:48 +0700

    qa update swaps test

--- qa/workspace/docker/swaps.py
+++ qa/workspace/docker/swaps.py
@@ -16,12 +16,13 @@ def mainloop(maker: object, taker: object, coin_a: str, coin_b: str, log: object
     time.sleep(time_sleep)
     price1 = rand_value(0.81, 0.995)  # gen prices and volumes for swap
     volume1 = rand_value(0.5, 0.9)
-    volume_to_swap = "{0:.8f}".format((Decimal(volume1)*Decimal(0.9))/swaps_to_run)
+    volume_to_swap = "{0:.8f}".format((Decimal(volume1) * Decimal(0.9)) / swaps_to_run + Decimal(0.00777))
     log.info("Creating maker order in %s s", str(time_sleep))
     res = maker.setprice(base=coin_a, rel=coin_b, price=price1, volume=volume1, cancel_previous=False)
     log.debug("Response: %s", str(res))
     for i in range(swaps_to_run):
         resp = taker.buy(base=coin_a, rel=coin_b, price=price1, volume=volume_to_swap)
+        assert not res.get('error')  # all orders should be successfully created
         log.debug("Create order, number: %s\n%s", str(i + 1), str(resp))
         if resp.get("result"):
             swap_uuids.append((resp.get("result")).get("uuid"))
